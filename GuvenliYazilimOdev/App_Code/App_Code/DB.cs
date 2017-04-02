9955fd982be3137f0068bbc7dfbca5b1a529e13b
meleren <meleren@yahoo.com>
2017-04-02 21:57:31 +0300

    Add files via upload

---
+++ GuvenliYazilimOdev/App_Code/App_Code/DB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+
+public class DB
+{
+    public static SqlConnection Connect()
+    {
+        string str = ConfigurationManager.ConnectionStrings["ConnStr"].ToString();
+        SqlConnection conn = new SqlConnection(str);
+        conn.Open();
+        return conn;
+    }
+
+    public static void Close(SqlConnection conn)
+    {
+        conn.Close();
+        conn.Dispose();
+    }
+}
