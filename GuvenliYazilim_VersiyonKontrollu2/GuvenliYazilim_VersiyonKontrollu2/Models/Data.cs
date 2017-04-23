53dd414745286cd314d332c9556c6ad26ecce288
meleren <meleren@yahoo.com>
2017-04-23 15:22:13 +0300

    Add files via upload

---
+++ GuvenliYazilim_VersiyonKontrollu2/GuvenliYazilim_VersiyonKontrollu2/Models/Data.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace GuvenliYazilim_VersiyonKontrollu2.Models
+{
+    public class Data
+    {
+        public class Urunler
+        {
+            [DataMember]
+            public IList<Urun> Liste { get; set; }
+        }
+
+        [DataContract]
+        public class Urun
+        {
+            [DataMember]
+            public int Id { get; set; }
+
+
+            [DataMember]
+            public string Tanim { get; set; }
+
+            [DataMember]
+            public string Birim { get; set; }
+
+            [DataMember]
+            public double BirimFiyat { get; set; }
+
+            [DataMember]
+            public string Marka { get; set; }
+
+        }
+        [DataContract]
+        public class UrunSiparis
+        {
+
+            [DataMember]
+            public int Id { get; set; }
+            [DataMember]
+            public int BayiId { get; set; }
+
+            [DataMember]
+            public int UrunId { get; set; }
+
+            [DataMember]
+            public string Birim { get; set; }
+
+            [DataMember]
+            public double BirimFiyat { get; set; }
+
+            [DataMember]
+            public double Miktar { get; set; }
+
+        }
+        [DataContract]
+        public class Urunxx
+        {
+            [DataMember]
+            public int Id { get; set; }
+
+
+            [DataMember]
+            public string Tanim { get; set; }
+
+            [DataMember]
+            public string Birim { get; set; }
+
+            [DataMember]
+            public double BirimFiyat { get; set; }
+
+
+        }
+
+        [DataContract]
+        public class DonenDeger
+        {
+            [DataMember]
+            public int Id { get; set; }
+
+
+            [DataMember]
+            public string Durum { get; set; }
+        }
+        public class GelenDeger
+        {
+            [DataMember]
+            public int Id { get; set; }
+
+        }
+
+        public class Siparisler
+        {
+            [DataMember]
+            public IList<Siparis> Liste { get; set; }
+        }
+        public class SiparislerToplami
+        {
+            [DataMember]
+            public IList<SiparisToplam> Liste { get; set; }
+
+        }
+
+        [DataContract]
+        public class Siparis
+        {
+            [DataMember]
+            public int SiparisNo { get; set; }
+
+            [DataMember]
+            public int Id { get; set; }
+
+            [DataMember]
+            public DateTime Tarih { get; set; }
+
+            [DataMember]
+            public Int16 BayiId { get; set; }
+
+            [DataMember]
+            public string BayiAdi { get; set; }
+
+            [DataMember]
+            public string Adres { get; set; }
+
+            [DataMember]
+            public Int16 UrunId { get; set; }
+
+            [DataMember]
+            public string UrunAdi { get; set; }
+
+            [DataMember]
+            public string Birim { get; set; }
+
+            [DataMember]
+            public double BirimFiyat { get; set; }
+
+            [DataMember]
+            public double Miktar { get; set; }
+
+            [DataMember]
+            public double Tutar { get; set; }
+
+        }
+        [DataContract]
+        public class SiparisToplam
+        {
+            [DataMember]
+            public int SiparisNo { get; set; }
+
+            [DataMember]
+            public DateTime Tarih { get; set; }
+
+            [DataMember]
+            public Int16 BayiId { get; set; }
+
+            [DataMember]
+            public string BayiAdi { get; set; }
+
+            [DataMember]
+            public double Tutar { get; set; }
+
+        }
+        [DataContract]
+        public class FaturaToplam
+        {
+            [DataMember]
+            public int FaturaNo { get; set; }
+
+            [DataMember]
+            public DateTime Tarih { get; set; }
+
+            [DataMember]
+            public Int16 BayiId { get; set; }
+
+            [DataMember]
+            public string BayiAdi { get; set; }
+
+            [DataMember]
+            public double Tutar { get; set; }
+
+        }
+        public class FaturalarToplam
+        {
+            [DataMember]
+            public IList<FaturaToplam> Liste { get; set; }
+
+        }
+
+        [DataContract]
+        public class Fatura
+        {
+            [DataMember]
+            public int FaturaNo { get; set; }
+
+            [DataMember]
+            public int SiparisNo { get; set; }
+
+            [DataMember]
+            public int Id { get; set; }
+
+            [DataMember]
+            public DateTime Tarih { get; set; }
+
+            [DataMember]
+            public Int16 BayiId { get; set; }
+
+            [DataMember]
+            public string BayiAdi { get; set; }
+            [DataMember]
+            public string Adres { get; set; }
+
+            [DataMember]
+            public string VergiDaire { get; set; }
+
+            [DataMember]
+            public string VergiNo { get; set; }
+
+            [DataMember]
+            public Int16 UrunId { get; set; }
+
+            [DataMember]
+            public string UrunAdi { get; set; }
+
+            [DataMember]
+            public string Birim { get; set; }
+
+            [DataMember]
+            public double BirimFiyat { get; set; }
+
+            [DataMember]
+            public double Miktar { get; set; }
+
+            [DataMember]
+            public double Tutar { get; set; }
+
+        }
+
+        public class Faturalar
+        {
+            [DataMember]
+            public IList<Fatura> Liste { get; set; }
+        }
+
+    }
+}
---
+++ GuvenliYazilim_VersiyonKontrollu2/GuvenliYazilim_VersiyonKontrollu2/obj/Release/Package/PackageTmp/Login.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="GuvenliYazilim_VersiyonKontrollu2.Login" %>
+
+
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  
+    <link href="css/style.css" rel="stylesheet" />
+
+  
+</head>
+
+<body>
+  
+<form runat="server">
+  <header>Kırtasiye Giriş Sayfası</header>
+  <label>Kullanıcı Adı <span>*</span></label>
+  <asp:TextBox ID="TxtKullanici" runat="server" CssClass="TextBox"></asp:TextBox>
+
+  <label>Şifre <span>*</span></label>
+  <asp:TextBox ID="TxtSifre" runat="server" TextMode="Password"  CssClass="TextBox"></asp:TextBox>
+    <asp:Button ID="BtnLogin" runat="server" Text="Giriş" CssClass="buttonx"  OnClick="BtnLogin_Click" />
+</form>
+  
+  
+</body>
+</html>
